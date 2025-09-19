95ebc750135fce5bbeae85fc77663e546b015d91
Ivriel <gunawanivriel@gmail.com>
2025-09-19 11:32:55 +0700

    Create add blogpost page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CategoryList } from './features/category/category-list/category-list';
 import { AddCategory } from './features/category/add-category/add-category';
 import { EditCategory } from './features/category/edit-category/edit-category';
 import { BlogpostList } from './features/blog-post/blogpost-list/blogpost-list';
+import { BlogpostAdd } from './features/blog-post/blogpost-add/blogpost-add';
 
 export const routes: Routes = [
     {
@@ -21,5 +22,9 @@ export const routes: Routes = [
         path:'admin/blogposts',
         component:BlogpostList
     },
+    {
+        path:'admin/blogposts/add',
+        component:BlogpostAdd
+    }
     
 ];
