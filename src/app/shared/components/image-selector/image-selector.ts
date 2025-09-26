affeb66b4afb71f441ade2df75d467d70b6d229a
Ivriel <gunawanivriel@gmail.com>
2025-09-26 16:58:32 +0700

    progress sampai bikin api + ui upload images

--- src/app/shared/components/image-selector/image-selector.ts
+++ src/app/shared/components/image-selector/image-selector.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ImageService } from './image';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../../models/blog-image.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-image-selector',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './image-selector.html',
   styleUrl: './image-selector.css'
 })
-export class ImageSelector {
+export class ImageSelector implements OnInit{
+  private file?:File;
+  fileName:string = '';
+  title:string = '';
+  images$?:Observable<BlogImage[]>
 
+  @ViewChild('form',{static:false}) imageUploadForm?:NgForm 
+
+  constructor(private imageService:ImageService){}
+
+  ngOnInit(): void {
+   this.getImages()
+  }
+
+    onFileUploadChange(event:Event):void {
+      const element = event.currentTarget as HTMLInputElement
+      this.file = element.files?.[0]
+    }
+
+    uploadImage():void {
+      if(this.file && this.fileName !== '' && this.title !== '') {
+        this.imageService.uploadImage(this.file,this.fileName,this.title).subscribe({
+          next:(res)=> {
+            this.imageUploadForm?.resetForm()
+            this.getImages()
+          }
+        })
+      }
+    }
+
+    private getImages(){
+      this.images$ = this.imageService.getAllImages()
+    }
 }
